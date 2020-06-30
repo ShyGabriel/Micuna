8d406333f9e96c5fec868dbfa0aa74862e287028
sssebastian <ubaldosebastian25@gmail.com>
2020-06-30 02:20:01 -0500

    Barra de menu inferior comiteada como con tu vieja

--- app/src/main/java/com/example/micuna/Contenido.java
+++ app/src/main/java/com/example/micuna/Contenido.java
@@ -1,14 +1,68 @@
 package com.example.micuna;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.micuna.fragments.HomeFragment;
+import com.example.micuna.fragments.OrderFragment;
+import com.example.micuna.fragments.ProfileFragment;
+import com.example.micuna.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Contenido extends AppCompatActivity {
+
+    BottomNavigationView mBottonNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenido);
+
+        //FRAGMENT PANTALLA DE INICIO
+        showSelectedFragment(new HomeFragment());
+
+        mBottonNavigation = (BottomNavigationView) findViewById(R.id.botonNavigation);
+
+        mBottonNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.menu_home){
+                    showSelectedFragment(new HomeFragment());
+                }
+
+                if (menuItem.getItemId() == R.id.menu_search){
+                    showSelectedFragment(new SearchFragment());
+                }
+
+                if (menuItem.getItemId() == R.id.menu_order){
+                    showSelectedFragment(new OrderFragment());
+                }
+
+                if (menuItem.getItemId() == R.id.menu_profile){
+                    showSelectedFragment(new ProfileFragment());
+                }
+
+                return true;
+            }
+        });
     }
+
+             /* ♦ METODO QUE PERMTE ELEGIR EL FRAGMENT ♦*/
+
+    private void showSelectedFragment(Fragment fragment){
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment)
+        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+
+
+    }
+
 }
